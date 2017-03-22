f40459c5571d016c22cfa7d777f69dba62c73dcb
Caio Valente <caio-valente@hotmail.com>
2017-03-22 14:17:43 +0100

    changing system from TCP to UDP

--- client.py
+++ client.py
@@ -5,96 +5,46 @@ from sys import argv
 import threading
 
 address_server = ('localhost', 1212)
+UDPsocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
 
 ''' thread functions '''
 
+
 # used by user interface thread
-def main_loop():
+def send_data():
+	print("Type messages to send: \t")
 	while 1:
-		msg = input("Type message to send: \t")
+		msg = input("")
 		msg = msg.encode('utf-8')
-		s.send(msg)
+		UDPsocket.sendto(msg, address_server)
 
 
 # used by server listener thread
-def listen_server():
+def receive_data():
 	print('listening server')
 	while 1:
-		received_msg = s.recv(1024)
-		print('Received:', received_msg.decode())
+		try:
+			received_data, addr = UDPsocket.recvfrom(1024)
+			print('Server:', received_data.decode())
+		except:
+			continue
 
 
-def run(s):
-	thread_listen = threading.Thread(target=listen_server)
-	thread_listen.daemon = True
-	thread_listen.start()
-
-	thread_user = threading.Thread(target=main_loop)
+def run_threads():
+	thread_user = threading.Thread(target=send_data)
 	thread_user.daemon = True
 	thread_user.start()
+
+	thread_listen = threading.Thread(target=receive_data)
+	thread_listen.daemon = True
+	thread_listen.start()
 
 	# hang program execution
 	while 1:
 		sleep(10)
 
 
-''' internal functions '''
-
-
-# create client socket
-def start_client(address_client):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(address_client)
-	return s
-
-
-# connect client to server
-def connect(s, address_server):
-	print('Trying to connect...')
-	s.connect(address_server)
-	print('Connected to server:', address_server)
-
-
 ''' main interface '''
 if __name__ == '__main__':
-	try:
-		address_client = (argv[1], int(argv[2]) )
-	except:
-		address_server = ('localhost', 1212)
-		address_client = ('localhost', 1414)
-	s = start_client(address_client)
-	connect(s, address_server)
-	run(s)
-
-
-
-''' sender
-
-# Echo client program
-import socket
-from time import sleep
-from sys import argv
-
-
-def start_client(ip='localhost', server_port=1212):
-	address_server = (ip, server_port)
-	address_client = ('localhost', 1313)
-
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(address_client)
-
-	print('Trying to connect...')
-	s.connect(address_server)
-	print('Connected to server:', address_server)
-
-	received_msg = s.recv(1024)
-	print('Received:', received_msg.decode())
-
-	while 1:
-		msg = input('type something to send it: ')
-		msg = msg.encode('utf-8')
-		s.send(msg)
-
-
-
-'''
+	run_threads()
--- server.py
+++ server.py
@@ -5,62 +5,76 @@ import threading
 import messages
 
 clients = []
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+UDPSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_dress = ('localhost', 1212)
 
 
 # add client to clients list
-def add_client(sock, addr):
-	client = {'id': len(clients), 'socket': sock, 'addr': addr}
+def add_client(addr):
+	# avoid adding clients already added
+	for client in clients:
+		if client['addr'] == addr: 	# can't compair using 'is'
+			return
+
+	client = {'id': len(clients), 'addr': addr}
 	clients.append(client)
 	print('Connected to a new client: \t', client['id'], client['addr'])
 
 
+# receive a normal string, code it and send to receivers list
 def send_msg(msg, receivers):
 	msg = msg.encode('utf-8')
 	for client in receivers:
-		client['socket'].sendall(msg)
+		UDPSock.sendto(msg, client['addr'])
 		print("Sent msg '" + msg.decode() + "' to client " + str(client['id']))
 
 
 def start_server():
-	# instantiate socket and bind it to localhost
-	address = ('localhost', 1212)
-	server_socket.bind(address)
-	print("Server started at address", address)
-	return server_socket
+	UDPSock.bind(server_dress)
+	print("Server started at address", server_dress)
 
 
 ''' thread functions '''
 
-# used by connection listener thread
-# listen to connection requests and accept them
-def listen_connection_requests():
+
+def receive_data():
 	while 1:
-		# listen to connection requests and accept them
-		server_socket.listen(1)
-		conn, addr = server_socket.accept()
-		add_client(conn, addr)
+		# receive message
+		received_data, addr = UDPSock.recvfrom(1024)
+		if not received_data: break
+
+		# add client to clients list
+		add_client(addr)
+
+		# generate answer and encode it
+		reponse = 'Received "' + received_data.decode() + '"'
+		print(reponse, 'from', addr)
+
+		# send answer
+		send_msg(reponse, clients)
 
 
-def main_loop():
-	while 1:
+def send_data():
+	pass
+	'''
+		while 1:
 		for client in clients:
-			received_data = client['socket'].recv(1024).decode()
+			received_data = client['socket'].recvfrom(1024).decode()
 			if not received_data: break
 			print('Received:', received_data)
 			send_msg(received_data, clients)
+	'''
 
+def run_threads():
+	# start a thread to receive data
+	sender_thread = threading.Thread(target=receive_data)
+	sender_thread.daemon = True
+	sender_thread.start()
 
-def run(s):
-	# start a thread to hang connection requests
-	thread_connection = threading.Thread(target=listen_connection_requests)
-	thread_connection.daemon = True
-	thread_connection.start()
-
-	# start a thread to hang connected users demands
-	thread_main = threading.Thread(target=main_loop)
-	thread_main.daemon = True
-	thread_main.start()
+	# start a thread to hang sending messages
+	sender_thread = threading.Thread(target=send_data)
+	sender_thread.daemon = True
+	sender_thread.start()
 
 	# hang program execution
 	while 1:
@@ -68,5 +82,5 @@ def run(s):
 
 
 if __name__ == '__main__':
-	server_socket = start_server()
-	run(server_socket)
+	start_server()
+	run_threads()
