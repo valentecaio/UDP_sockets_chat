6b55e0dc672de869048743722703723754a4b291
Caio Valente <caio-valente@hotmail.com>
2017-03-27 02:48:08 +0200

    bugfix in DISCONNECT; refactor comments and prints; added try/catch to client keyboard looping

--- client.py
+++ client.py
@@ -63,129 +63,133 @@ def read_keyboard():
 	global self_state
 	print("Type messages to send: \t")
 	while 1:
-		user_input = input("")
-		user_cmd = user_input.split(' ')[0]
-		print('command ' + user_cmd)
+		try:
+			user_input = input("> ")
+			user_cmd = user_input.split(' ')[0]
+			#print('command ' + user_cmd)
 
-		if user_cmd == CMD_HELP:
-			print(	'\t%s to show this help,\n'
-				  	'\t%s <text> to send a message,\n'
-				  	'\t%s to connect to server\n'
-					'\t%s to get the users list\n'
-					'\t%s <group type> <member_1 id>....<member_n id> to create a private group (0=centr. and 1=decentr.)\n'
-					'\t%s <group id> to accept the invitation of this group\n'
-					'\t%s <group id> to reject the invitation of this group\n'
-					'\t%s to leave private group and join the public group again\n'
-					'\t%s to disconnect\n'
-				  % (CMD_HELP,CMD_SEND,CMD_CONNECT,CMD_USER_LIST, CMD_CREATE_GROUP, CMD_ACCEPT_INVITATION, CMD_REJECT_INVITATION, CMD_DISJOINT, CMD_DISCONNECT))
+			if user_cmd == CMD_HELP:
+				print(	'\t%s to show this help,\n'
+						'\t%s <text> to send a message,\n'
+						'\t%s to connect to server\n'
+						'\t%s to get the users list\n'
+						'\t%s <group type> <member_1 id>....<member_n id> to create a private group (0=centr. and 1=decentr.)\n'
+						'\t%s <group id> to accept the invitation of this group\n'
+						'\t%s <group id> to reject the invitation of this group\n'
+						'\t%s to leave private group and join the public group again\n'
+						'\t%s to disconnect\n'
+					  % (CMD_HELP,CMD_SEND,CMD_CONNECT,CMD_USER_LIST, CMD_CREATE_GROUP, CMD_ACCEPT_INVITATION, CMD_REJECT_INVITATION, CMD_DISJOINT, CMD_DISCONNECT))
 
-		elif user_cmd == CMD_PRINT:
-			print("ID: %s, group: %s, state: %s,\n"
-				  % (self_id, group_users[self_id]['group'], self_state))
-			pprint(group_users)
-
-		elif user_cmd == CMD_CONNECT:
-			# abort if already connected
-			if self_state is not ST_DISCONNECTED:
-				print("You can't use this command because you're already connected")
-				continue
-
-			# abort if username is too long
-			username = user_input[len(CMD_CONNECT) + 1:].strip()
-			if len(username) > 8:
-				print('Your username can not contain more than 8 characters. '
-					  'Please choose another one.')
-				continue
-
-			msg = m.createConnectionRequest(0, username)
-			UDPsocket.sendto(msg, address_server)
-
-		else:
-			# abort others commands if not connected
-			if self_state is not ST_CONNECTED:
-				print("You can't use this command because you're not connected")
-				continue
-
-			if user_cmd == CMD_SEND:
-				text = user_input[len(CMD_SEND)+1:].encode('utf-8')
-				msg = m.createDataMessage(0, self_id, group_users[self_id]['group'], text)
-				UDPsocket.sendto(msg, address_server)
-
-			elif user_cmd == CMD_DISCONNECT:
-				msg = m.disconnectionRequest(0, self_id)
-				UDPsocket.sendto(msg, address_server)
-
-			elif user_cmd == CMD_USER_LIST:
+			elif user_cmd == CMD_PRINT:
+				print("ID: %s, group: %s, state: %s,\n"
+					  % (self_id, group_users[self_id]['group'], self_state))
 				pprint(group_users)
 
-			elif user_cmd == CMD_ACCEPT_INVITATION:
-				args, invalid_arg = getIntArgs(user_input)
-
-				group_id = args[0]
-				# verify if arguments are valid
-				if (len(args) < 1) or invalid_arg \
-						or (group_id not in group_invitations):
-					print("Usage:\n> %s <group id>\n"
-						  "Where <group id> must be a valid id" % (user_input))
+			elif user_cmd == CMD_CONNECT:
+				# abort if already connected
+				if self_state is not ST_DISCONNECTED:
+					print("You can't use this command because you're already connected")
 					continue
 
-
-				sender_id = group_invitations[group_id]['creator']
-				# create acceptation message and send it
-				group_type = group_invitations[group_id]['type']
-				accept = m.groupInvitationAccept(0, sender_id, group_type,
-												 group_id, self_id)
-				UDPsocket.sendto(accept, address_server)
-				del group_invitations[group_id]
-
-			elif user_cmd == CMD_REJECT_INVITATION:
-				args, invalid_arg = getIntArgs(user_input)
-				group_id = args[0]
-				# verify if arguments are valid
-				if (len(args) < 1) or invalid_arg \
-						or (group_id not in group_invitations):
-					print("Usage:\n> %s <group id>\n"
-						  "Where <group id> must be a valid id" % (user_input))
+				# abort if username is too long
+				username = user_input[len(CMD_CONNECT) + 1:].strip()
+				if len(username) > 8:
+					print('Your username can not contain more than 8 characters. '
+						  'Please choose another one.')
 					continue
 
-				# create rejection message and send it
-				sender_id = group_invitations[group_id]['creator']
-				group_type = group_invitations[group_id]['type']
-				reject = m.groupInvitationReject(0,sender_id, group_type,
-												 group_id, self_id)
-				UDPsocket.sendto(reject, address_server)
-				del group_invitations[group_id]
-
-			elif user_cmd == CMD_CREATE_GROUP:
-				args, invalid_arg = getIntArgs(user_input)
-
-				# verify if arguments are valid
-				if (len(args) < 2) or (args[0] not in [0,1]) or invalid_arg:
-					print("Usage:\n> %s <group type> <member 1> <member 2> ... "
-						  "<member N>\nWhere <group type> must be 0 for "
-						  "centralized or 1 for decentralized\n" % (user_input))
-					continue
-
-				# create request
-				msg = m.groupCreationRequest(0, self_id, args[0], args[1:])
+				msg = m.createConnectionRequest(0, username)
 				UDPsocket.sendto(msg, address_server)
 
-			elif user_cmd == CMD_DISJOINT:
-				if group_users[self_id]['group'] == 1:
-					print('You are already in the public group.')
+			else:
+				# abort others commands if not connected
+				if self_state is not ST_CONNECTED:
+					print("You can't use this command because you're not connected")
+					continue
+
+				if user_cmd == CMD_SEND:
+					text = user_input[len(CMD_SEND)+1:].encode('utf-8')
+					msg = m.createDataMessage(0, self_id, group_users[self_id]['group'], text)
+					UDPsocket.sendto(msg, address_server)
+
+				elif user_cmd == CMD_DISCONNECT:
+					msg = m.disconnectionRequest(0, self_id)
+					UDPsocket.sendto(msg, address_server)
+
+				elif user_cmd == CMD_USER_LIST:
+					pprint(group_users)
+
+				elif user_cmd == CMD_ACCEPT_INVITATION:
+					args, invalid_arg = getIntArgs(user_input)
+
+					group_id = args[0]
+					# verify if arguments are valid
+					if (len(args) < 1) or invalid_arg \
+							or (group_id not in group_invitations):
+						print("Usage:\n> %s <group id>\n"
+							  "Where <group id> must be a valid id" % (user_input))
+						continue
+
+
+					sender_id = group_invitations[group_id]['creator']
+					# create acceptation message and send it
+					group_type = group_invitations[group_id]['type']
+					accept = m.groupInvitationAccept(0, sender_id, group_type,
+													 group_id, self_id)
+					UDPsocket.sendto(accept, address_server)
+					del group_invitations[group_id]
+
+				elif user_cmd == CMD_REJECT_INVITATION:
+					args, invalid_arg = getIntArgs(user_input)
+					group_id = args[0]
+					# verify if arguments are valid
+					if (len(args) < 1) or invalid_arg \
+							or (group_id not in group_invitations):
+						print("Usage:\n> %s <group id>\n"
+							  "Where <group id> must be a valid id" % (user_input))
+						continue
+
+					# create rejection message and send it
+					sender_id = group_invitations[group_id]['creator']
+					group_type = group_invitations[group_id]['type']
+					reject = m.groupInvitationReject(0,sender_id, group_type,
+													 group_id, self_id)
+					UDPsocket.sendto(reject, address_server)
+					del group_invitations[group_id]
+
+				elif user_cmd == CMD_CREATE_GROUP:
+					args, invalid_arg = getIntArgs(user_input)
+
+					# verify if arguments are valid
+					if (len(args) < 2) or (args[0] not in [0,1]) or invalid_arg:
+						print("Usage:\n> %s <group type> <member 1> <member 2> ... "
+							  "<member N>\nWhere <group type> must be 0 for "
+							  "centralized or 1 for decentralized\n" % (user_input))
+						continue
+
+					# create request
+					msg = m.groupCreationRequest(0, self_id, args[0], args[1:])
+					UDPsocket.sendto(msg, address_server)
+
+				elif user_cmd == CMD_DISJOINT:
+					if group_users[self_id]['group'] == 1:
+						print('You are already in the public group.')
+
+					else:
+						#send disjoint request
+						disjoint_request = m.groupDisjointRequest(0, self_id)
+						UDPsocket.sendto(disjoint_request, address_server)
+
 
 				else:
-					#send disjoint request
-					disjoint_request = m.groupDisjointRequest(0, self_id)
-					UDPsocket.sendto(disjoint_request, address_server)
-
-
-			else:
-				print("This is not a valid command. Type "
-					  + CMD_HELP + " to get some help.")
-				continue
-
-		#UDPsocket.sendto(msg, address_server)
+					print("This is not a valid command. Type "
+						  + CMD_HELP + " to get some help.")
+					continue
+		except:
+			# hide errors if disconnected
+			if self_state is not ST_DISCONNECTED:
+				print(traceback.format_exc())
+			continue
 
 
 # used by server listener thread
@@ -223,7 +227,7 @@ def main_loop():
 				if msg_type == m.TYPE_CONNECTION_ACCEPT:
 					self_id = m.unpack_connection_accept_content(data)
 
-					print("Connected to CENTRALIZED GROUP with id %s" + str(self_id))
+					print("Connected to PUBLIC GROUP with id " + str(self_id))
 					self_state = ST_CONNECTED
 
 					# send Acknowledgment as response
@@ -268,10 +272,10 @@ def main_loop():
 					print('Changes in the user list. Type "USERS" to see changes')
 
 				elif msg_type == m.TYPE_UPDATE_DISCONNECTION:
-					self_id = m.unpack_connection_accept_content(data)
+					client_id = m.unpack_connection_accept_content(data)
 
-					username = group_users[self_id]['username']
-					del group_users[self_id]
+					username = group_users[client_id]['username']
+					del group_users[client_id]
 
 					#send Acknowledgment
 					response = m.acknowledgement(msg_type, 0, self_id)
